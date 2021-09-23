6ac10cd45d1322f9eabc1fe4ad537a941b0bff68
Saksham Munjal <sakshammunjal4@gmail.com>
2021-09-23 11:11:12 +0530

    Backend:  User Auth

---
+++ backend/controllers/userController.js
@@ -0,0 +1,110 @@
+import asynchandler from 'express-async-handler'
+import generateToken from '../utils/generateToken.js'
+import User from '../models/userModel.js'
+
+//@desc Auth User and get token
+//@route POST /api/users/login
+//@access public
+
+const authUser = asynchandler(async (req,res) => {
+    const {email,password} = req.body;
+
+    const user = await User.findOne({email});
+
+    if(user && (await user.matchPassword(password))) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            age: user.age,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(401);
+        throw new Error("Invalid email or password");
+    }
+})
+
+//@desc Register User
+//@route POST /api/users
+//@access public
+
+const registerUser = asynchandler(async (req,res) => {
+    const {name, email, password, age} = req.body;
+
+    const userExists = await User.findOne({ email });
+
+    if(userExists){
+        res.status(400);
+        throw new Error('Email already registered');
+    }
+
+    const user = await User.create({
+        name, email, password, age
+    });
+
+    if (user) {
+        res.status(201).json({
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          age: user.age,
+          token: generateToken(user._id),
+        });
+      } else {
+        res.status(400);
+        throw new Error("Invalid user data");
+      }
+})
+
+//@desc GET User profile
+//@route POST /api/users/profile
+//@access private
+
+const getUserProfile = asynchandler(async (req,res) => {
+    const user = await User.findById(req.user._id);
+
+    if(user){
+        res.json({
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          age: user.age
+        })
+    } else {
+        res.status(404);
+        throw new Error('User Not found');
+    }
+})
+
+
+//@desc Update User profile
+//@route PUT /api/users/profile
+//@access private
+
+const updateUserProfile = asynchandler (async (req,res) => {
+    const user = await User.findById(req.user._id);
+
+    if(user){
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+        user.age = req.body.age || user.age;
+        if(req.body.password) {
+            user.password = req.body.password
+        }
+
+        const updatedUser = await user.save();
+        res.json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            age: updatedUser.age,
+            token: generateToken(updatedUser._id),
+          });
+    } else {
+        res.status(404);
+        throw new Error("User not found");
+    }
+})
+
+export {authUser, registerUser, getUserProfile, updateUserProfile}
---
+++ backend/server.js
@@ -0,0 +1,32 @@
+import path from "path";
+import  express  from "express";
+import dotenv from 'dotenv';
+import userRoutes from './routes/userRoutes.js';
+import connectDB from './config/db.js'
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+app.use(express.json());
+
+app.use('/api/users', userRoutes);
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/funds/build')))
+
+    app.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    )
+  } else {
+    app.get('/', (req, res) => {
+      res.send('API is running....')
+    })
+  }
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log("Server Started at Port 5000"))
